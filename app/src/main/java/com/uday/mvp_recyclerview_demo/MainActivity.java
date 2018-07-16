8b8151f95b9b596b48ca0b26018cc581744974fa
Uday Singh <uday144@gmail.com>
2018-07-16 12:59:29 +0530

    First commit

---
+++ app/src/main/java/com/uday/mvp_recyclerview_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uday.mvp_recyclerview_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
